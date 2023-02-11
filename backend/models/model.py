c3ca2cbda780a9cabc7c45c8b11f699e023334c6
AnisShaikh10 <anisofficial0208@gmail.com>
2023-02-11 16:14:17 +0530

    added megerd with Dinesh branch

--- backend/models/model.py
+++ backend/models/model.py
@@ -16,6 +16,22 @@ class Orders(BaseModel):
     status: str
     date: datetime
 
+class OrderList(BaseModel):
+    user_name: str = Field(...)
+    items: list = Field(...)
+    grand_total: int = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        schema_extra = {
+            "example": {
+                "user_name": "Jane Doe",
+                "items": [{"item_name": "samosa", "quantity":1, "price": 0, "total": 1 * 1}],
+                "grand_total": 0
+            }
+        }
+
 class Tables(BaseModel):
     table_no: int
     capicity: int
